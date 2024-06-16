3837c284c2cff3091e8c42a6192d78ba4ad03d45
Alina Vlasova <alina_vlasova_01@bk.ru>
2024-06-16 10:53:09 +0300

    update user module

--- src/controllers/users/users.module.ts
+++ src/controllers/users/users.module.ts
@@ -5,11 +5,19 @@ import {AppService} from "../../app.service";
 import {UsersService} from "../../services/users/users.service";
 import {MongooseModule} from "@nestjs/mongoose";
 import {User, UserSchema} from "../../schema/users";
+import {AuthService} from "../../services/authorization/auth/auth.service";
+import {PassportModule} from "@nestjs/passport";
+import {JwtModule} from "@nestjs/jwt";
+import {jwtConstants} from "../../static/private/constans";
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+        PassportModule,
+        JwtModule.register({
+            secret: jwtConstants.secret
+        })],
     controllers: [UsersController],
-    providers: [UsersService],
+    providers: [UsersService, AuthService],
 })
 export class UsersModule {
 }
